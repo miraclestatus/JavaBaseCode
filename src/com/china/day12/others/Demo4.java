e291ceadc9de7842cc9161ebfa5f354a027c9ba8
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-03 20:55:37 +0800

    1111

--- src/com/china/day12/others/Demo4.java
+++ src/com/china/day12/others/Demo4.java
@@ -1,6 +1,7 @@
 package com.china.day12.others;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,7 +10,7 @@ import java.util.Date;
  * @date 2020/8/3 20:26
  */
 public class Demo4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // 日期格式化类 simpledateformat 类
         // 我们经常会看到一些不同的日期格式
 
@@ -22,5 +23,18 @@ public class Demo4 {
         System.out.println(s);
         String s1 = sdf2.format(date);
         System.out.println(s1);
+        // format 将date类型转化为指定的字符串类型
+        // 那么如何将字符串的日期形式转化成 date类型呢
+        // parse 方法进行解析
+        String str = "2008年08月08日";
+        // 将str转化成时间类型
+        DateFormat sdf3 = new SimpleDateFormat("yyyy年MM月dd日");
+        Date date1 = sdf3.parse(str);
+        System.out.println(date1); // Fri Aug 08 00:00:00 CST 2008
+        //  日期转化成字符串使用  SimpleDateFormat(字符串格式).format(目标字符串)
+        // 字符串 转化日期   SimpleDateFormat(日期格式).parse(date类型)
+
+
+
     }
 }
---
+++ src/com/china/day12/others/Demo5.java
@@ -0,0 +1,17 @@
+package com.china.day12.others;
+
+import java.util.Calendar;
+
+/**
+ * @author Eric Lee
+ * @date 2020/8/3 20:43
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+        // calendar 日历类 比date更为常用
+        // 创建一个 Calendar类
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar);
+
+    }
+}
