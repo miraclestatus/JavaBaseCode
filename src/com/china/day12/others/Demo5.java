8fa0a9cb33b08a66e65125f3874d932a383e6b4b
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-03 21:32:28 +0800

    1111

--- src/com/china/day12/others/Demo5.java
+++ src/com/china/day12/others/Demo5.java
@@ -12,6 +12,12 @@ public class Demo5 {
         // 创建一个 Calendar类
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar);
-
+        // 获取指定的年
+        int year = calendar.get(Calendar.YEAR);
+        // 获取指定的月 (0~11)共12月
+        int month = calendar.get(Calendar.MONTH)+1;
+        // 获取指定的日
+        int day =  calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(year  + "年" + month +"月" +day+ "日");
     }
 }
